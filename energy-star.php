77c348ae9c268bd5346d720bf49f23048248e49a
Phan Văn Huy Hoàng <pvhuyhoang166@gmail.com>
2017-01-14 10:48:34 +0700

    update energy-star page

--- energy-star.php
+++ energy-star.php
@@ -1,193 +1,139 @@
 <?php require 'include/header.php';?>
+<div class="page_content">
 
 
-<!----------------------------------- start banner ----------------------------------------------->
-<div class="banner" style="background:url('images/energy1.jpg'); background-size: cover;" >
-	<div class="container">
-		<div class="banner-content">
-			<h1 class="banner-title">warm up with energy star</h1>
-		</div>
-	</div>
-</div>
-<!----------------------------------- end banner ------------------------------------------------------>
-
-<div class="space-custom"></div>
-<div class="container">
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13940651.084857471!2d-104.7478563828105!3d64.75305065058342!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x53b00f5201b6604b%3A0x7cbffe9e3b1508e5!2s19+Riedel+St%2C+Fort+McMurray%2C+AB+T9H%2C+Ca-na-%C4%91a!5e0!3m2!1svi!2s!4v1484363854191" width="100%" height="960" frameborder="0" style="border:0" allowfullscreen></iframe>
+<div class="container" id="center_content">
+	
 	<div class="row mt6 mb6">
-		<div class="col-md-7 col-sm-7">
-			<div class="first-text">
-				<span class="hc-subtitle">Choose a Light with</span><br/>
-				<h3 class="hc-title">ENERGY STAR</h3>
-				<div class="hc-text pt2">
-				<p>Use our Choose a Light Guide to find an ENERGY STAR bulb<br>
-with the perfect shape, color, and brightness for your home.</p>
-				</div>
-			</div>
-			<div class="mt3 mb3">
-					<a href="#" class="main-fill small-btn">Learn More &raquo;</a>
-			</div>
-		</div>
-		<div class="col-md-5 col-sm-5 energy-img">
-			<img src="images/energy2.jpg" class="img-responsive">	
-			<div class="energy-img-txt">
-				<p>Independently certified to deliver efficiency and performance</p>
-			</div>
-		
-		</div>
+		<form action="" class="choose_address" id="choose_address">
+			<input type="text" class="input_address border_style" placeholder="Enter your Address"/>
+			<select name="choiceProduce" id="choiceProduce" class="border_style">
+				<option value="">Produce Of Choice</option>
+				<option value="Slow">Slow</option>
+				<option selected="selected">Medium</option>
+				<option>Fast</option>
+				<option>Faster</option>
+		    </select>
+			<button type="submit" class="input_find border_style"><i class="fa fa-search"></i>Find</button>
+		</form>
 	</div>
-	<div class="container">
-		<div class="row">
-			<div class="col-sm-4">
-				<span class="hc-subtitle">Home</span><br/>
-				<h3 class="hc-title">Improvement</h3>
+	<div class="row ">
+		<div class="col-md-4 install_area_box border_style">
+			<p class="intall_area">Installer in your area</p>
+			<div class="intall_area_head">
+				<h4 class="area_info">Bell - 0.2 km</h5>
+				<p>Clearwater Landing</p>
+				<p>19 Riedel St.</p>
+				<p>Fort McMurray,AB T9H5P8</p>
+				<p>9876543210</p>
 			</div>
-			<div class="col-sm-8">
-				<div class="a-line">
-					<p>Use the chart below to match your biggest concerns to the home improvement projects that may be able to address them and deliver the comfort and energy efficiency you are looking for.</p>
-				</div>
-			</div>
-		</div>
-		<div class="row mt3">
-			<div class="col-sm-4">
-				<div class="hs-section-box mt3  energy-img2">					
-					<img src="images/energy3.jpg" class="img-responsive">
-					<div class="energy-img-txt2 ">
-						<p>Sealing and Insulating</p>
-					</div>
-				</div>
-			</div>
+			<form action="" class="install_area" id="install_area">
+				<select name="choiceCites" id="choiceCites" class="border_style">
+					<option value="">Cities</option>
+					<option value="Slow">Slow</option>
+					<option selected="selected">Medium</option>
+					<option>Fast</option>
+					<option>Faster</option>
+			    </select>
+			    <input type="text" class="input_postal border_style" placeholder="Postal Code">
+			    <textarea class="input_des border_style" rows="5" id="comment" placeholder="Description"></textarea>
+				<button type="submit" class="input_send border_style">Send</button>
+			</form>
 
-			<div class="col-sm-4">
-				<div class="hs-section-box mt3  energy-img2">					
-					<img src="images/energy4.jpg" class="img-responsive">
-					<div class="energy-img-txt2 ">
-						<p>Heating and Cooling</p>
-					</div>
-				</div>
-			</div>
+		</div>
+	</div>
+	
+</div>	
+</div>
+<style>
+	.page_content{
+		position: relative;
+		margin-top: 80px;
+	}
+	.gm-style .place-card-large{
+		display: none!important;
+	}
+	#center_content{
+		position: absolute;
+		top: 0;
+		left: 50%;
+		transform: translateX(-50%);
+	}
+	.choose_address{
+		padding: 15px;
+		background-color: rgba(248, 247, 245, 0.75);
+	}
+	.choose_address select#choiceProduce{
+		padding-left: 15px;
+	}
+	.input_address{
+		padding: 10px 20px;
+		width: calc(100% - 331px);
+	}
+	.input_find {
+		color: #fff;
+		font-weight: bold;
+		text-align: center;
+		padding: 15px 50px;
+		background-color: #272323;
+	}
+	.input_find i{
+		margin-right: 10px;
+	}
+	.border_style{
+		border: 1px solid #be5b0b;
+		height: 50px;
+		margin-right: 5px;
+		border-radius: 2px;
+	}
+	.border_style:focus{
+		outline: 0;
+	}
+	.ui-widget.ui-widget-content{
+		border: 1px solid #be5b0b!important;
+	}
+	.install_area_box.border_style{
+		height: auto;
+		background-color: #fff;	
+	}
+	#choiceCites,.input_postal,.input_des,.input_send{
+		display: block;
+		width: 100%;
+		margin-bottom: 15px;
+	}
+	.input_send{
+		background: #be5b0b;
+		color: #fff;
+		text-transform: uppercase;
+		border-radius: 0;
+	}
+	.install_area_box .border_style{
+		height: 40px;
+		padding: 0 15px;
+	}
+	.install_area_box .input_des.border_style{
+		height: auto;
+	}
+	.intall_area{
+		text-align: center;
+		background: #be5b0b;
+		margin-left: -15px;
+		margin-right: -15px;
+		color: #fff;
+		line-height: 50px;
+	}
+	.intall_area_head{
+		margin-bottom: 15px;
+	}
+	h4.area_info{
+		color: #be5b0b;
+		font-weight: bold;
+	}
+	.intall_area_head p{
+		margin: 0;
+	}
+</style>
 
-			<div class="col-sm-4">
-				<div class="mt3 energy-img2">					
-					<img src="images/energy5.jpg" class="img-responsive">
-					<div class="energy-img-txt2 ">
-						<p>Home Performance with CANADIAN POWER SYSTEM</p>
-					</div>
-				</div>
-			</div>			
-			
-			
-		</div>
-	</div>
-</div>
-<div class="space-custom"></div>
-<div class="dark-section">
-	<div class="container">
-		<div class="row pt6 pb6">
-			<div class="col-sm-5">	
-				<div class="sbi-img">
-					<img src="images/energy6.png" class="img-responsive">
-					
-				</div>
-			</div>
-			<div class="col-sm-7">
-				<div>
-					<span class="dark-hc-subtitle">energy</span><br/>
-					<h3 class="dark-hc-title">EFFICIENCY</h3>
-					
-					<p class="mt4">Welcome to your personalized tool for saving energy with ENERGY STAR. Below are featured actions to help you get started. Create your own My ENERGY STAR account and see how your simple actions can make a big difference.</p>
-					<div class="mt6">
-						<a href="#" class="sc-main-fill small-btn">Learn More &raquo;</a>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
-<div class="container">
-	<div class="row mt3 mb4">
-		<div class="col-sm-4">
-			<span class="hc-subtitle">Energy Efficient</span><br/>
-			<h3 class="hc-title">Products</h3>
-		</div>
-		<div class="col-sm-8">
-			<div class="a-line">
-				<p>Find ENERGY STAR products, Learn new ways to save energy, Find tools to help you save, Get rebates and offers</p>
-			</div>
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-sm-3">
-			<div class="mt3 energy-img2">					
-				<img src="images/energy7.jpg" class="img-responsive">
-				<div class="energy-img-txt2 ">
-					<p>Appliances</p>
-				</div>
-			</div>
-		</div>
-		
-		<div class="col-sm-3">
-			<div class="mt3 energy-img2">					
-				<img src="images/energy8.jpg" class="img-responsive">
-				<div class="energy-img-txt2 ">
-					<p>Electronics</p>
-				</div>
-			</div>
-		</div>
-		
-		<div class="col-sm-3">
-			<div class="mt3 energy-img2">					
-				<img src="images/energy9.jpg" class="img-responsive">
-				<div class="energy-img-txt2 ">
-					<p>Office Equipment</p>
-				</div>
-			</div>
-		</div>
-		
-		<div class="col-sm-3">
-			<div class="mt3 energy-img2">					
-				<img src="images/energy10.jpg" class="img-responsive">
-				<div class="energy-img-txt2 ">
-					<p>Building Product</p>
-				</div>
-			</div>
-		</div>
-		
-		<div class="col-sm-3">
-			<div class="mt3 energy-img2">					
-				<img src="images/energy11.jpg" class="img-responsive">
-				<div class="energy-img-txt2 ">
-					<p>Heating and Cooling</p>
-				</div>
-			</div>
-		</div>
-		
-		<div class="col-sm-3">
-			<div class="mt3 energy-img2">					
-				<img src="images/energy12.jpg" class="img-responsive">
-				<div class="energy-img-txt2 ">
-					<p>Commercials Food Service Equipment</p>
-				</div>
-			</div>
-		</div>
-		
-		<div class="col-sm-3">
-			<div class="mt3 energy-img2">					
-				<img src="images/energy13.jpg" class="img-responsive">
-				<div class="energy-img-txt2 ">
-					<p>Lighting</p>
-				</div>
-			</div>
-		</div>
-		
-		<div class="col-sm-3">
-			<div class="mt3 energy-img2">					
-				<img src="images/energy14.jpg" class="img-responsive">
-				<div class="energy-img-txt2 ">
-					<p>Water Heaters</p>
-				</div>
-			</div>
-		</div>
-	</div>	
-</div>
-<div class="space-custom"></div>
+
 <?php require 'include/footer.php';?>
---
+++ energy-star_backup.php
@@ -0,0 +1,193 @@
+<?php require 'include/header.php';?>
+
+
+<!----------------------------------- start banner ----------------------------------------------->
+<div class="banner" style="background:url('images/energy1.jpg'); background-size: cover;" >
+	<div class="container">
+		<div class="banner-content">
+			<h1 class="banner-title">warm up with energy star</h1>
+		</div>
+	</div>
+</div>
+<!----------------------------------- end banner ------------------------------------------------------>
+
+<div class="space-custom"></div>
+<div class="container">
+	<div class="row mt6 mb6">
+		<div class="col-md-7 col-sm-7">
+			<div class="first-text">
+				<span class="hc-subtitle">Choose a Light with</span><br/>
+				<h3 class="hc-title">ENERGY STAR</h3>
+				<div class="hc-text pt2">
+				<p>Use our Choose a Light Guide to find an ENERGY STAR bulb<br>
+with the perfect shape, color, and brightness for your home.</p>
+				</div>
+			</div>
+			<div class="mt3 mb3">
+					<a href="#" class="main-fill small-btn">Learn More &raquo;</a>
+			</div>
+		</div>
+		<div class="col-md-5 col-sm-5 energy-img">
+			<img src="images/energy2.jpg" class="img-responsive">	
+			<div class="energy-img-txt">
+				<p>Independently certified to deliver efficiency and performance</p>
+			</div>
+		
+		</div>
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-4">
+				<span class="hc-subtitle">Home</span><br/>
+				<h3 class="hc-title">Improvement</h3>
+			</div>
+			<div class="col-sm-8">
+				<div class="a-line">
+					<p>Use the chart below to match your biggest concerns to the home improvement projects that may be able to address them and deliver the comfort and energy efficiency you are looking for.</p>
+				</div>
+			</div>
+		</div>
+		<div class="row mt3">
+			<div class="col-sm-4">
+				<div class="hs-section-box mt3  energy-img2">					
+					<img src="images/energy3.jpg" class="img-responsive">
+					<div class="energy-img-txt2 ">
+						<p>Sealing and Insulating</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-sm-4">
+				<div class="hs-section-box mt3  energy-img2">					
+					<img src="images/energy4.jpg" class="img-responsive">
+					<div class="energy-img-txt2 ">
+						<p>Heating and Cooling</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-sm-4">
+				<div class="mt3 energy-img2">					
+					<img src="images/energy5.jpg" class="img-responsive">
+					<div class="energy-img-txt2 ">
+						<p>Home Performance with CANADIAN POWER SYSTEM</p>
+					</div>
+				</div>
+			</div>			
+			
+			
+		</div>
+	</div>
+</div>
+<div class="space-custom"></div>
+<div class="dark-section">
+	<div class="container">
+		<div class="row pt6 pb6">
+			<div class="col-sm-5">	
+				<div class="sbi-img">
+					<img src="images/energy6.png" class="img-responsive">
+					
+				</div>
+			</div>
+			<div class="col-sm-7">
+				<div>
+					<span class="dark-hc-subtitle">energy</span><br/>
+					<h3 class="dark-hc-title">EFFICIENCY</h3>
+					
+					<p class="mt4">Welcome to your personalized tool for saving energy with ENERGY STAR. Below are featured actions to help you get started. Create your own My ENERGY STAR account and see how your simple actions can make a big difference.</p>
+					<div class="mt6">
+						<a href="#" class="sc-main-fill small-btn">Learn More &raquo;</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="container">
+	<div class="row mt3 mb4">
+		<div class="col-sm-4">
+			<span class="hc-subtitle">Energy Efficient</span><br/>
+			<h3 class="hc-title">Products</h3>
+		</div>
+		<div class="col-sm-8">
+			<div class="a-line">
+				<p>Find ENERGY STAR products, Learn new ways to save energy, Find tools to help you save, Get rebates and offers</p>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-sm-3">
+			<div class="mt3 energy-img2">					
+				<img src="images/energy7.jpg" class="img-responsive">
+				<div class="energy-img-txt2 ">
+					<p>Appliances</p>
+				</div>
+			</div>
+		</div>
+		
+		<div class="col-sm-3">
+			<div class="mt3 energy-img2">					
+				<img src="images/energy8.jpg" class="img-responsive">
+				<div class="energy-img-txt2 ">
+					<p>Electronics</p>
+				</div>
+			</div>
+		</div>
+		
+		<div class="col-sm-3">
+			<div class="mt3 energy-img2">					
+				<img src="images/energy9.jpg" class="img-responsive">
+				<div class="energy-img-txt2 ">
+					<p>Office Equipment</p>
+				</div>
+			</div>
+		</div>
+		
+		<div class="col-sm-3">
+			<div class="mt3 energy-img2">					
+				<img src="images/energy10.jpg" class="img-responsive">
+				<div class="energy-img-txt2 ">
+					<p>Building Product</p>
+				</div>
+			</div>
+		</div>
+		
+		<div class="col-sm-3">
+			<div class="mt3 energy-img2">					
+				<img src="images/energy11.jpg" class="img-responsive">
+				<div class="energy-img-txt2 ">
+					<p>Heating and Cooling</p>
+				</div>
+			</div>
+		</div>
+		
+		<div class="col-sm-3">
+			<div class="mt3 energy-img2">					
+				<img src="images/energy12.jpg" class="img-responsive">
+				<div class="energy-img-txt2 ">
+					<p>Commercials Food Service Equipment</p>
+				</div>
+			</div>
+		</div>
+		
+		<div class="col-sm-3">
+			<div class="mt3 energy-img2">					
+				<img src="images/energy13.jpg" class="img-responsive">
+				<div class="energy-img-txt2 ">
+					<p>Lighting</p>
+				</div>
+			</div>
+		</div>
+		
+		<div class="col-sm-3">
+			<div class="mt3 energy-img2">					
+				<img src="images/energy14.jpg" class="img-responsive">
+				<div class="energy-img-txt2 ">
+					<p>Water Heaters</p>
+				</div>
+			</div>
+		</div>
+	</div>	
+</div>
+<div class="space-custom"></div>
+<?php require 'include/footer.php';?>
